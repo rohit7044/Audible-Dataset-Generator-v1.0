2e7c85046aaee4d23530c638901e5d733325f4b5
ROHIT DAS <rdas.879@gmail.com>
2021-05-30 20:28:02 +0530

    Update audible_data_handling.py

--- src/audible_data_handling.py
+++ src/audible_data_handling.py
@@ -5,7 +5,7 @@ import re
 final_data_list = []
 book_data_list = []
 final_reviews_list = []
-book_data_columns = ["Book Title","Book Subtitle","Book Author", "Book Narrator", "Audio Runtime", "Audiobook_Type", "Categories" , "Rating", "Price"]
+book_data_columns = ["Book Title","Book Subtitle","Book Author", "Book Narrator", "Audio Runtime", "Audiobook_Type", "Categories" , "Rating","Total No. of Ratings", "Price"]
 book_review_columns = []
 total_review_count =""
 final_column_data =[]
@@ -27,14 +27,16 @@ def write_to_csv(book_data_list,final_reviews_list):
     final_data_list = book_data_list + final_reviews_list
     csv_file_path = "output_encoded.csv"
 
+
     try:
         if os.path.exists(csv_file_path):
             if os.path.getsize(csv_file_path) is 0:
                 csv_file = open(csv_file_path, "w", encoding="utf-8",newline ="")
                 writer = csv.writer(csv_file, delimiter = ",")
-                
+
                 writer.writerow(final_column_data)
-                writer.writerow(final_data_list)          
+                writer.writerow(final_data_list)
+
             else:
                 csv_file = open(csv_file_path, "a", encoding="utf-8", newline="")
                 writer = csv.writer(csv_file, delimiter=",")
@@ -49,43 +51,6 @@ def write_to_csv(book_data_list,final_reviews_list):
         csv_file.close()
     except Exception as e:
         print(e)
-    # try:
-    #     if os.path.exists(txt_file_path):
-    #         if os.path.getsize(txt_file_path) is 0:
-    #             txt_file = open(txt_file_path, "w", encoding=None)
-    #
-    #             for data in range(len(final_column_data)):
-    #                 txt_file.write(final_column_data[data]+",")
-    #                 if data == len(final_column_data) - 1:
-    #                     txt_file.write("\n")
-    #             for data in range(len(final_data_list)):
-    #                 temp = "\""+final_data_list[data]+"\""+","
-    #                 txt_file.write("\""+final_data_list[data]+"\""+",")
-    #                 if data == len(final_data_list) - 1:
-    #                     txt_file.write("\n")
-    #         else:
-    #             txt_file = open(txt_file_path, "a", encoding=None)
-    #
-    #             for data in range(len(final_data_list)):
-    #                 temp = "\"" + final_data_list[data] + "\"" + ","
-    #                 txt_file.write("\"" + final_data_list[data] + "\"" + ",")
-    #                 if data == len(final_data_list) - 1:
-    #                     txt_file.write("\n")
-    #     else:
-    #         txt_file = open(txt_file_path, "w", encoding=None)
-    #
-    #         for data in range(len(final_column_data)):
-    #             txt_file.write(final_column_data[data] + ",")
-    #             if data == len(final_column_data) - 1:
-    #                 txt_file.write("\n")
-    #         for data in range(len(final_data_list)):
-    #             temp = "\"" + final_data_list[data] + "\"" + ","
-    #             txt_file.write("\"" + final_data_list[data] + "\"" + ",")
-    #             if data == len(final_data_list) - 1:
-    #                 txt_file.write("\n")
-    #     txt_file.close()
-    # except Exception as e:
-    #     print(e)
 
     return
 def review_column_creator(review_list):
